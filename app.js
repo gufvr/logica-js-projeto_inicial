7ccf8df1b1eeaa336bafee573420e9cba8a17c2b
GuFvr <gustavo.vfavero@gmail.com>
2024-11-18 21:29:38 -0300

    feat: implement attempts counter and prompt retry logic

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 alert('Boas vindas ao jogo do n.° secreto');
-let secretNumber = 4;
+let secretNumber = 5;
 console.log(secretNumber);
+let kick;
+let attempts = 1;
 
-let kick = prompt('Escolha um n.° entre 1 e 10');
+while (kick != secretNumber) {
+  kick = prompt('Escolha um n.° entre 1 e 10');
 
-if (kick == secretNumber) {
-  alert(`Isso aí, você acertou! O número secreto era ${secretNumber}`);
-} else {
-  if (secretNumber > kick) {
-    alert(`O número secreto é maior que ${kick}`);
-  } else if (secretNumber < kick) {
-    alert(`O número secreto é menor que ${kick}`);
+  if (kick == secretNumber) {
+    break;
+  } else {
+    if (secretNumber > kick) {
+      alert(`O número secreto é maior que ${kick}`);
+    } else if (secretNumber < kick) {
+      alert(`O número secreto é menor que ${kick}`);
+    }
+    attempts++;
   }
 }
+let attemptWord = attempts > 1 ? 'tentativas' : 'tentativa';
+alert(
+  `Isso aí, você acertou! O número secreto era ${secretNumber}. Você acertou com ${attempts} ${attemptWord}.`
+);
